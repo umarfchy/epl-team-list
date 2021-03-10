3b331030d99193acef505d9cba6119b13716bafb
umar <unzippinglinux@gmail.com>
2021-03-10 20:11:29 +0600

    fetch success

--- epl-team-info/src/App.js
+++ epl-team-info/src/App.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>
-    {
-      console.log('Hello')
-    }
-  </div>;
+  let [apiData, setApiData] = useState([]);
+
+  const apiLink =
+    "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League";
+
+  // fetchig api data
+  useEffect(() => {
+    fetch(apiLink)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+  }, []);
+
+  console.log(apiData);
+  return (
+    <div>
+      {apiData.teams.map((team) => (
+        <h3>{team.strTeam}</h3>
+      ))}
+    </div>
+  );
 }
 
 export default App;
